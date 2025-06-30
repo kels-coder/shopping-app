e609b45062d67d6e8e419c65409e6e5b09c2b0fc
kels-coder <kelsgamerkg@gmail.com>
2025-07-01 00:12:18 +0530

    added a dummy product list

---
+++ lib/models/dummy_products.dart
@@ -0,0 +1,22 @@
+import 'package:shopping_app/models/products.dart';
+
+final List<Product> dummyProducts = [
+  Product(
+    id: '1',
+    name: 'Tomato',
+    imageUrl: 'https://thumbs.dreamstime.com/b/tomesoral-191754454.jpg?w=768',
+    price: 50,
+  ),
+  Product(
+    id: '2',
+    name: 'Tomato',
+    imageUrl: 'https://thumbs.dreamstime.com/b/tomesoral-191754454.jpg?w=768',
+    price: 40,
+  ),
+  Product(
+    id: '3',
+    name: 'Tomato',
+    imageUrl: 'https://thumbs.dreamstime.com/b/tomesoral-191754454.jpg?w=768',
+    price: 30,
+  ),
+];
