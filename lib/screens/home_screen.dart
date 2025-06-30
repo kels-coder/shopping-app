0d30eab8771e862e3068e9f3853682271c961803
kels-coder <kelsgamerkg@gmail.com>
2025-07-01 00:34:03 +0530

    added a seperate appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:shopping_app/screens/cart_screen.dart';
-import 'package:shopping_app/screens/profile_screen.dart';
+import 'package:shopping_app/screens/appbar_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -14,100 +12,8 @@ class HomeScreen extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-        title: Padding(
-          padding: EdgeInsets.only(right: screenWidth < 400 ? 20 : 40),
-          child: Text(
-            'Organic Basket',
-            style: GoogleFonts.pacifico(
-              textStyle: TextStyle(
-                fontSize: screenWidth < 400 ? 22 : 26,
-                color: Colors.white,
-                shadows: const [
-                  Shadow(
-                    blurRadius: 8,
-                    color: Colors.black54,
-                    offset: Offset(2, 2),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        actions: [
-          const SizedBox(width: 12),
-          Row(
-            children: [
-              const SizedBox(width: 6),
-              GestureDetector(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (ctx) => const ProfileScreen()),
-                  );
-                },
-
-                child: Row(
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(builder: (ctx) => CartScreen()),
-                        );
-                      },
-                      icon: Icon(Icons.shopping_cart),
-                    ),
-                    Text(
-                      'Profile',
-                      style: GoogleFonts.pacifico(
-                        textStyle: TextStyle(
-                          fontSize: screenWidth < 400 ? 16 : 14,
-                          fontWeight: FontWeight.w500,
-                          color: Colors.white,
-                          shadows: const [
-                            Shadow(
-                              blurRadius: 8,
-                              color: Colors.black54,
-                              offset: Offset(2, 2),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(width: 6),
-                    CircleAvatar(
-                      radius: 18, // Larger avatar
-                      backgroundColor: Colors.white.withAlpha(
-                        80,
-                      ), // Slightly more solid
-                      child: Text(
-                        username.isNotEmpty ? username[0].toUpperCase() : '?',
-                        style: TextStyle(
-                          backgroundColor: Theme.of(
-                            context,
-                          ).colorScheme.secondaryContainer,
-
-                          fontSize: 16, // Slightly bigger font
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
-      body: Padding(
-        padding: EdgeInsets.only(
-          top: MediaQuery.of(context).padding.top + kToolbarHeight,
-        ),
-        child: const Center(
-          child: Text('This is the HomeScreen', style: TextStyle(fontSize: 20)),
-        ),
-      ),
+      appBar: buildAppBar(context, username),
+      body: Text('This is the HomeScreen'),
     );
   }
 }
